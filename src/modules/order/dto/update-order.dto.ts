fd9feb4e53195d894773ba2db75610ebd8ede169
JavierApaza7991 <japazah@unsa.edu.pe>
2023-04-14 18:33:46 -0500

    Ajuste en order

--- src/modules/order/dto/update-order.dto.ts
+++ src/modules/order/dto/update-order.dto.ts
@@ -1,7 +1,7 @@
 import OrderAttributes from '@albatrosdeveloper/ave-models-npm/lib/schemas/order/order.entity';
 import { PartialType } from '@nestjs/mapped-types';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsOptional, IsNotEmpty } from 'class-validator';
+import { IsString, IsOptional, IsNotEmpty, IsBoolean } from 'class-validator';
 
 export class UpdateOrderDto extends PartialType(OrderAttributes) {
   @ApiProperty({
@@ -33,4 +33,25 @@ export class UpdatePaymentSessionIdDto {
   @IsString()
   @IsNotEmpty()
   paymentSessionId: string;
+
+  @ApiProperty({
+    required: false,
+  })
+  @IsBoolean()
+  @IsOptional()
+  flagRTN?: boolean;
+
+  @ApiProperty({
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  businessName?: string;
+
+  @ApiProperty({
+      required: false,
+  })
+  @IsString()
+  @IsOptional()
+  RTN?: string;
 }
